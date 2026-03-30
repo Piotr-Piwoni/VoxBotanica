5d9e1c9ccd41571e8ece489892986275fff154b1
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2026-03-30 17:23:17 +0100

    Set up Basic UI Functionality.
    - Made the panel hide itself based on a button press.
    - Created a UIManager class responsible for connecting the UI with the generator.

---
+++ Assets/_Root/_Scripts/Runtime/Systems/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace VoxBotanica.Systems
+{
+public class UIManager : MonoBehaviour
+{
+	private UIDocument _Document;
+	private VisualElement _Root;
+
+
+	private void Awake()
+	{
+		_Document = GetComponent<UIDocument>();
+		_Root = _Document?.rootVisualElement;
+	}
+
+	private void Start()
+	{
+		var panel = _Root.Q<GroupBox>();
+		var panelButton = _Root.Q<Button>();
+
+		panelButton.clicked += () =>
+		{
+			float panelWidth = panel.resolvedStyle.width + 10f;
+
+			bool isClosed = _Root.ClassListContains("closed");
+
+			if (isClosed)
+			{
+				_Root.RemoveFromClassList("closed");
+				panelButton.text = "◀";
+				_Root.style.translate = new Translate(0, 0, 0);
+			}
+			else
+			{
+				_Root.AddToClassList("closed");
+				panelButton.text = "▶";
+				_Root.style.translate = new Translate(-panelWidth, 0, 0);
+			}
+		};
+	}
+}
+}

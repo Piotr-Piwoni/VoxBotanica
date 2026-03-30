a4e01a2ca5a8174b0da6ecba8e92f0041f7b1717
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2026-03-30 03:10:27 +0100

    Created Basic ModelExporter Class.
    - Implemented basic FBX exporting.

--- Assets/_Root/_Scripts/Runtime/Systems/ModelExporter.cs
+++ Assets/_Root/_Scripts/Runtime/Systems/ModelExporter.cs
@@ -1,6 +1,39 @@
+using System.IO;
+using Autodesk.Fbx;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace VoxBotanica
 {
-public class ModelExporter : MonoBehaviour { }
+public class ModelExporter : MonoBehaviour
+{
+	[SerializeField]
+	private string _ExportFolderPath =
+			@"W:\Projects\Engines\Unity\Abertay\[CMP400] - Honours Project\VoxBotanica\Assets\_Root\Exports";
+	[SerializeField]
+	private Object _ExportObject;
+
+
+	[Button]
+	private void Export()
+	{
+		var fileName = $"{_ExportObject.name}.fbx";
+		string filePath = Path.Combine(_ExportFolderPath, fileName);
+
+		using (var fbxManager = FbxManager.Create())
+		{
+			fbxManager.SetIOSettings(FbxIOSettings.Create(fbxManager, Globals.IOSROOT));
+
+			using (var exporter = FbxExporter.Create(fbxManager, "myExporter"))
+			{
+				bool status = exporter.Initialize(fileName, -1,
+												  fbxManager.GetIOSettings());
+
+				var scene = FbxScene.Create(fbxManager, "myScene");
+
+				exporter.Export(scene);
+			}
+		}
+	}
+}
 }

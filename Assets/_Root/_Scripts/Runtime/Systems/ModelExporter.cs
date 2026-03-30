b52a7a24cc5bc60b7bdf3d2050534c3b13b0b322
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2026-03-30 03:27:16 +0100

    Updated to ModelExporter with Proper FBX Exporting.

--- Assets/_Root/_Scripts/Runtime/Systems/ModelExporter.cs
+++ Assets/_Root/_Scripts/Runtime/Systems/ModelExporter.cs
@@ -11,29 +11,66 @@ public class ModelExporter : MonoBehaviour
 	private string _ExportFolderPath =
 			@"W:\Projects\Engines\Unity\Abertay\[CMP400] - Honours Project\VoxBotanica\Assets\_Root\Exports";
 	[SerializeField]
-	private Object _ExportObject;
+	private GameObject _ExportObject;
 
 
 	[Button]
 	private void Export()
 	{
-		var fileName = $"{_ExportObject.name}.fbx";
-		string filePath = Path.Combine(_ExportFolderPath, fileName);
+		string filePath = Path.Combine(_ExportFolderPath, $"{_ExportObject.name}.fbx");
 
-		using (var fbxManager = FbxManager.Create())
+		var meshFilter = _ExportObject.GetComponent<MeshFilter>();
+		if (!meshFilter)
 		{
-			fbxManager.SetIOSettings(FbxIOSettings.Create(fbxManager, Globals.IOSROOT));
+			Debug.LogError("No MeshFilter Found!");
+			return;
+		}
 
-			using (var exporter = FbxExporter.Create(fbxManager, "myExporter"))
+		Mesh mesh = meshFilter.sharedMesh;
+
+
+		using (var manager = FbxManager.Create())
+		{
+			manager.SetIOSettings(FbxIOSettings.Create(manager, Globals.IOSROOT));
+
+			using (var exporter = FbxExporter.Create(manager, "Exporter"))
 			{
-				bool status = exporter.Initialize(fileName, -1,
-												  fbxManager.GetIOSettings());
+				if (!exporter.Initialize(filePath, -1, manager.GetIOSettings()))
+				{
+					Debug.LogError("Failed to initialize exporter!");
+					return;
+				}
 
-				var scene = FbxScene.Create(fbxManager, "myScene");
+				var scene = FbxScene.Create(manager, "Scene");
+				var node = FbxNode.Create(scene, _ExportObject.name);
+				var fbxMesh = FbxMesh.Create(scene, "Mesh");
+
+				Vector3[] vertices = mesh.vertices;
+				int[] triangles = mesh.triangles;
+
+				fbxMesh.InitControlPoints(vertices.Length);
+				for (var i = 0; i < vertices.Length; i++)
+					fbxMesh.SetControlPointAt(new FbxVector4(vertices[i].x,
+												  vertices[i].y,
+												  vertices[i].z), i);
+
+				for (int i = 0; i < triangles.Length; i += 3)
+				{
+					fbxMesh.BeginPolygon();
+					fbxMesh.AddPolygon(triangles[i]);
+					fbxMesh.AddPolygon(triangles[i + 1]);
+					fbxMesh.AddPolygon(triangles[i + 2]);
+					fbxMesh.EndPolygon();
+				}
+
+				node.SetNodeAttribute(fbxMesh);
+				scene.GetRootNode().AddChild(node);
 
 				exporter.Export(scene);
 			}
 		}
+
+		Debug.Log("Exported FBX to: " + filePath);
 	}
 }
 }
